e68b95f5bffa73b6e5dd2484a7eb8160832fe1fc
Jolan <jolan.wagner@gmail.com>
2025-06-02 16:35:57 +0200

    🟢 Update from home : Removing unused struct in .h

--- inc/minitalk.h
+++ inc/minitalk.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jowagner <jowagner@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jolanwagner13 <jolanwagner13@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 11:29:33 by jolanwagner       #+#    #+#             */
-/*   Updated: 2025/05/27 14:48:01 by jowagner         ###   ########.fr       */
+/*   Updated: 2025/06/02 16:34:59 by jolanwagner      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,6 @@
 # include "../libft/inc/libft.h"
 # include <signal.h>
 # include <sys/types.h>
-
-typedef struct s_message
-{
-	unsigned char	current_char;
-	int				bit_position;
-}					t_message;
 
 volatile sig_atomic_t	g_signal_received = 1;
 
